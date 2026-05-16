0f5a28767e2d158219e72d7583562e74807bfe57
Zeynep Tuncel <zeynepqtuncel@gmail.com>
2026-05-16 03:57:29 +0300

    Çalışan ekleme ve görüntüleme fonksiyonları eklendi

--- okul_otomasyon_sistemi.py
+++ okul_otomasyon_sistemi.py
@@ -31,6 +31,7 @@ def ogrenci_ekle():
         dosya.write(f"{isim},{soyisim},{tc},{sinif},{bolum}\n")
     print(f"Öğrenci kaydedildi. İsim: {isim} Soyisim: {soyisim} Tc: {tc} Sınıf: {sinif} Bölüm: {bolum}")
     ogrenci=Ogrenci(isim,soyisim,tc,sinif,bolum)
+
     
 def ogrenci_goruntule():
     goruntulenecek_tc=input("Bilgileirni görüntülemek istediğiniz öğrencinin tcsinin giriniz: ")
@@ -49,7 +50,7 @@ def ogrenci_goruntule():
         else:
             tc=bilgiler[2]
             if goruntulenecek_tc==tc:
-                print(f"İsim: {bilgiler[0]} Soyisim: {bilgiler[1]}\nTc: {bilgiler[2]}\nSınıf: {bilgiler[3]}\nBölüm: {bilgiler[4]}")
+                print(f"\nİsim: {bilgiler[0]} Soyisim: {bilgiler[1]}\nTc: {bilgiler[2]}\nSınıf: {bilgiler[3]}\nBölüm: {bilgiler[4]}")
                 bulundu_mu=True
                 break
     if bulundu_mu==False:
@@ -85,7 +86,38 @@ def ogrenci_sil():
         else:
             print("Öğrenci bulunamadı")
     
+def calisan_ekle():
+    isim=input("Çalışan ismi giriniz: ").lower()
+    soyisim=input("Çalışan soyismini giriniz: ").lower()
+    tc = input("Çalışan tc kimlik numarasını giriniz: ")
+    meslek = input("Çalışan mesleğini giriniz: ").lower()
+    maas = input("Çalışan maas bilgisini giriniz: ")
+    with open("calisan_bilgileri.txt","a",encoding="utf-8") as dosya:
+        dosya.write(f"{isim},{soyisim},{tc},{meslek},{maas}\n")
+    calisan=Calisan(isim,soyisim,tc,meslek,maas)
+    print(f"Çalışan kaydedildi. İsim: {isim} Soyisim: {soyisim} Tc: {tc} Meslek: {meslek} Maaş: {maas}")
 
+def calisan_goruntule():
+    goruntulencek_tc=input("Görüntülemek istediginiz çalışanın tcsini giriniz: ")
+    with open("calisan_bilgileri.txt","r",encoding="utf-8") as dosya:
+        satirlar=dosya.readlines()
+        bulundu_mu=False
+        for satir in satirlar:
+            bilgiler=satir.split(",")
+            if not(len(bilgiler)==5):
+                if goruntulencek_tc in satir:
+                    print(len(bilgiler))
+                    bulundu_mu=True
+                    print("Çalışan bulunamadı\nVeri dosyasında hata var kontrol edin.")
+                    break
+            else:
+                tc=bilgiler[2]
+                if tc==goruntulencek_tc:
+                    bulundu_mu=True
+                    print(f"\nİsim: {bilgiler[0]} Soyisim: {bilgiler[1]}\nTc: {bilgiler[2]}\nMeslek: {bilgiler[3]}\nMaaş: {bilgiler[4]}")
+                    break
+    if bulundu_mu==False:
+        print("Bu tc ile çalışan bulunamamıştır")
 
 
 print("-----HOŞ GELDİNİZ-----")
@@ -100,28 +132,40 @@ if kullanici_adi=="admin" and sifre=="12345":
     "2-Çalışan İşlemleri\n" \
     "Seçiminiz: ")
 
-    if secim=="1":  #Ogrenci İslem Menüsü
+    if secim=="1":  
         print("---Öğrenci İşlem Menüsü---")
         while True:
-            secim=input("1-Yeni Öğrenci Ekle"
-            "2-Öğrenci Bilgi Görüntüle"
-            "3-Öğrenci Sil"
-            "4-Çıkış"
+            secim=input("\n1-Yeni Öğrenci Ekle\n"
+            "2-Öğrenci Bilgi Görüntüle\n"
+            "3-Öğrenci Sil\n"
+            "4-Çıkış\n"
             "seçiminiz: ")
             if secim=="1":
                 ogrenci_ekle()
-            elif secim=="2":
+            if secim=="2":
                 ogrenci_goruntule()
-            elif secim=="3":
+            if secim=="3":
                 ogrenci_sil()
+            if secim== "4":
+                break
+
+
     if secim=="2":
         print("---Çalışan İşlem Menüsü---")
-        secim=input("1-Yeni Çalışan Ekle"
-            "2-Çalışan Bilgi Görüntüle"
-            "3-Çalışan Sil"
-            "4-Çıkış"
+        while True:
+            secim=input("\n1-Yeni Çalışan Ekle\n"
+            "2-Çalışan Bilgi Görüntüle\n"
+            "3-Çalışan Sil\n"
+            "4-Çıkış\n"
             "seçiminiz: ")
-        
+            if secim=="1":
+                calisan_ekle()
+            if secim=="2":
+                calisan_goruntule()
+            if secim=="4":
+                break
+            
+             
 
 
 

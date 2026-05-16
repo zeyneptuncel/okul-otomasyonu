bdc03754ce8c7c9a116014368770ba9457569758
Zeynep Tuncel <zeynepqtuncel@gmail.com>
2026-05-16 03:13:47 +0300

    Öğrenci bilgi görüntüleme özelliği eklendi

--- okul_otomasyon_sistemi.py
+++ okul_otomasyon_sistemi.py
@@ -29,10 +29,63 @@ def ogrenci_ekle():
 
     with open ("ogrenci_bilgileri.txt","a",encoding="utf-8") as dosya:
         dosya.write(f"{isim},{soyisim},{tc},{sinif},{bolum}\n")
-        print(f"Öğrenci kaydedildi. İsim: {isim} Soyisim: {soyisim} Tc: {tc} Sınıf: {sinif} Bölüm: {bolum}")
-
-
+    print(f"Öğrenci kaydedildi. İsim: {isim} Soyisim: {soyisim} Tc: {tc} Sınıf: {sinif} Bölüm: {bolum}")
+    ogrenci=Ogrenci(isim,soyisim,tc,sinif,bolum)
     
+def ogrenci_goruntule():
+    goruntulenecek_tc=input("Bilgileirni görüntülemek istediğiniz öğrencinin tcsinin giriniz: ")
+    with open("ogrenci_bilgileri.txt","r",encoding="utf-8") as dosya:
+        satirlar= dosya.readlines()
+    bulundu_mu=False
+    for satir in satirlar:
+        bilgiler=satir.split(",")
+        if not(len(bilgiler)>=4):
+            if goruntulenecek_tc in satir:
+                bulundu_mu=True
+                print("Öğrenci bulunamadı\nVeri dosyasında hata var kontrol edin.")
+                break
+            else:
+                continue
+        else:
+            tc=bilgiler[2]
+            if goruntulenecek_tc==tc:
+                print(f"İsim: {bilgiler[0]} Soyisim: {bilgiler[1]}\nTc: {bilgiler[2]}\nSınıf: {bilgiler[3]}\nBölüm: {bilgiler[4]}")
+                bulundu_mu=True
+                break
+    if bulundu_mu==False:
+        print("Bu tcye ait öğrenci bulunamadı.")
+        
+def ogrenci_sil():
+    silinecek_tc=input("Silmek istediğiniz öğrenci tcsini giriniz: ")
+    with open("ogrenci_bilgileri.txt","r", encoding="utf-8") as dosya:
+        satirlar=dosya.readlines()
+    with open("ogrenci_bilgileri.txt","w", encoding="utf-8") as dosya:
+        silindi_mi=False
+        hata=False
+        for satir in satirlar:
+            bilgiler=satir.split(",")
+            if not(len(bilgiler)>=4):
+                if silinecek_tc in satir:
+                    hata=True
+                else:
+                    dosya.write(satir)
+            else:
+                tc=bilgiler[2]
+                if tc!=silinecek_tc:
+                    dosya.write(satir)
+                else:
+                    silinen_ad=bilgiler[0]
+                    silinen_soyad=bilgiler[1]
+                    silindi_mi=True
+ 
+        if hata==True:
+            print("Veri hatası olan satır başarıyla silindi")
+        if silindi_mi==True:
+            print(f"{silinen_ad} {silinen_soyad} isimli öğrencinin kaydı başarıyla silindi")
+        else:
+            print("Öğrenci bulunamadı")
+    
+
 
 
 print("-----HOŞ GELDİNİZ-----")
@@ -46,15 +99,32 @@ if kullanici_adi=="admin" and sifre=="12345":
     secim=input("1-Öğrenci İşlemleri\n" \
     "2-Çalışan İşlemleri\n" \
     "Seçiminiz: ")
-    if secim=="1":
+
+    if secim=="1":  #Ogrenci İslem Menüsü
         print("---Öğrenci İşlem Menüsü---")
-        secim=input("1-Yeni Öğrenci Ekle"
-        "2-Öğrenci Bilgi Görüntüle"
-        "3-Öğrenci Sil"
-        "4-Çıkış"
-        "seçiminiz: ")
-        if secim=="1":
-            ogrenci_ekle()
+        while True:
+            secim=input("1-Yeni Öğrenci Ekle"
+            "2-Öğrenci Bilgi Görüntüle"
+            "3-Öğrenci Sil"
+            "4-Çıkış"
+            "seçiminiz: ")
+            if secim=="1":
+                ogrenci_ekle()
+            elif secim=="2":
+                ogrenci_goruntule()
+            elif secim=="3":
+                ogrenci_sil()
+    if secim=="2":
+        print("---Çalışan İşlem Menüsü---")
+        secim=input("1-Yeni Çalışan Ekle"
+            "2-Çalışan Bilgi Görüntüle"
+            "3-Çalışan Sil"
+            "4-Çıkış"
+            "seçiminiz: ")
+        
+
+
+
 
 
             
